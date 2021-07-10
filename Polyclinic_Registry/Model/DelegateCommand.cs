6a363aadad03e735b9a7937dcefcd88351f7b5d3
Kirill Abent <blayner0027@gmail.com>
2021-07-10 17:39:35 +0400

    Написал модель

---
+++ Polyclinic_Registry/Model/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Polyclinic_Registry.Model
+{
+	public class DelegateCommand : ICommand
+	{
+		private Action<object> execute;
+		private Func<object, bool> canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			this.execute = execute;
+			this.canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter) => canExecute == null || canExecute(parameter);
+
+		public void Execute(object parameter) => execute(parameter);
+	}
+}
---
+++ Polyclinic_Registry/Model/Patient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Polyclinic_Registry.Model
+{
+	//Пациент
+	public class Patient
+	{
+		public string FullName { get; init; }
+		public string Address { get; init; }
+		public string Phone { get; init; }
+
+		public Patient(string fullName, string address, string phone)
+		{
+			if (!string.IsNullOrWhiteSpace(fullName) || !string.IsNullOrWhiteSpace(address) ||
+			    !string.IsNullOrWhiteSpace(phone))
+			{
+				FullName = fullName;
+				Address = address;
+				Phone = phone;
+			}
+			else
+			{
+				throw new ArgumentNullException();
+			}
+		}
+		/// <summary>
+		/// Для сериализации в Json
+		/// </summary>
+		public Patient(){}
+	}
+}
